3549076a705e2037fe0c7c268495e4b6c611eb63
Ruslan Akbashev <ruslashev@gmail.com>
2020-10-03 06:30:20 +0300

    parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let filename = parse_arguments();
+    println!("filename={}", filename);
 }
+
+fn parse_arguments() -> String {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        usage();
+        std::process::exit(1);
+    }
+
+    args[1].clone()
+}
+
+fn usage() {
+    println!("Usage: elfcat <filename>")
+}
+
