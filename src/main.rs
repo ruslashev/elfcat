5328670ed4e0f322f30e0d3321ebd6d0a1514af4
Ruslan Akbashev <ruslashev@gmail.com>
2020-10-03 10:30:39 +0300

    write reports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::Write;
 use std::path::Path;
 
 struct ParsedElf {
@@ -11,6 +12,8 @@ fn main() {
     let elf = ParsedElf::from_bytes(&filename, contents);
     let report_filename = construct_filename(&filename);
     let report = generate_report(&elf);
+
+    std::fs::write(report_filename, report).expect("failed to write report");
 }
 
 fn parse_arguments() -> String {
@@ -59,10 +62,18 @@ fn construct_filename(filename: &String) -> String {
     stem(basename(filename)).to_string() + ".html"
 }
 
-fn generate_report(elf: &ParsedElf) -> String {
-    let mut output: String = String::from("");
+macro_rules! w {
+    ($dst:expr, $($arg:tt)*) => (write!($dst, $( $arg )* ).unwrap())
+}
 
-    output.push_str("<!doctype html>\n");
+fn generate_report(elf: &ParsedElf) -> String {
+    let mut output = String::new();
+    let o = &mut output;
+
+    w!(o, "<!doctype html>\n");
+    w!(o, "<head>\n");
+    w!(o, "<title>{}</title>\n", basename(&elf.filename));
+    w!(o, "</head>\n");
 
     output
 }
