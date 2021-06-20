85275242737c5fd67505573acf452a86656a97ab
Ruslan Akbashev <ruslashev@gmail.com>
2021-06-21 00:39:29 +0300

    put offsets in one take to not slow browser to a crawl

--- src/js/offsets.js
+++ src/js/offsets.js
@@ -1,10 +1,12 @@
 function populateOffsets(columns) {
     let rows = Math.ceil(fileLen / columns);
-    let container = document.getElementById('offsets');
+    let elements = "";
     var offset = 0;
 
     for (var i = 0; i < rows; ++i) {
-        container.innerHTML += offset.toString(16) + "</br>\n";
+        elements += offset.toString(16) + "</br>\n";
         offset += columns;
     }
+
+    document.getElementById('offsets').innerHTML = elements;
 }
